75dac0fce559aa362a63928cbdc45038fe64c44d
Ashlie Gray <ashliejanegray@gmail.com>
2021-03-04 15:26:34 +1000

    added file to create database and user file for docker

---
+++ src/docker_postgres_init.sql
@@ -0,0 +1,11 @@
+CREATE USER test WITH PASSWORD 'test' CREATEDB;
+CREATE DATABASE test
+    WITH
+    OWNER = test
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = 1
+
+-- https://github.com/oxlb/docker-postgres-multiple-databases/blob/master/docker_postgres_init.sql
